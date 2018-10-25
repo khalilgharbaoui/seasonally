f6350bcc084d5ade20e02ea5f1cd81dc262a04ac
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-26 00:32:40 +0200

    Add and execute rake task for model annotation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id                 :bigint(8)        not null, primary key
+#  default_daily_rate :decimal(, )
+#  title              :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class Listing < ApplicationRecord
 end

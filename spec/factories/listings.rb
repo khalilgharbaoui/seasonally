fa5c679a7b4bd100e5d1d4b87b7c5f228b4c0cc4
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-26 16:05:54 +0200

    Add anotation to listings factory

--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id                 :bigint(8)        not null, primary key
+#  default_daily_rate :decimal(, )
+#  title              :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 FactoryBot.define do
   factory :listing do
     id { 1111 }

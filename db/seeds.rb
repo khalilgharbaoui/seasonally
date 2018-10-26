7e1c97b321dab908113e3a49c8e2682811b78e44
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-26 16:06:57 +0200

    Add 1001 seeds for testing in the browser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+1001.times.each do |n|
+  Listing.create!(title: "camper#{n+1}", default_daily_rate: 100 + n)
+  SeasonalRate.create!(listing_id: n+1, start_date: DateTime.new(2018,7,1),
+                                        end_date: DateTime.new(2018,9,1),
+                                        daily_rate: 150 + n)
+end
